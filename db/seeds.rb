5b4e8056929ea6eb767a02626128f7d05f27223b
maxdota <sieu_ngoc_2007@yahoo.com>
2013-09-30 13:34:45 +0700

    fixing heroku deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+User.create(username: 'n',
+  password_digest: '$2a$10$.z2zZtizY9MvE8.jKXd62uRe4MpSnCzqvdcVViLVnxKUw4vDU6aOm  ',
+  email_address: 'ngoc.nguyen@stanyangroup.com',
+  birthday: Date.parse('1992-09-30'),
+  account_creation_date: Date.parse('2013-09-30'),
+  activation: true,
+  role: 'normal',
+  phone: '0123456789',
+  full_name: 'Nguyen Hong Ngoc'
+  )
