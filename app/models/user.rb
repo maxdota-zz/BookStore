89cec3cbc5064833cfe0a06725450dc3f47e83c8
maxdota <sieu_ngoc_2007@yahoo.com>
2013-10-02 09:49:25 +0700

    [#58050638]let user reset password via token sent by email

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,20 +18,41 @@ class User < ActiveRecord::Base
   after_save :ensure_an_admin_remains
 
   def send_activation_email(name, account, email_address, activate_link)
+    send_email(email_address, 
+      "Max's Online Bookstore Account Activation",
+      "Welcome to Max's Online Bookstore, " + name + "
+        Please copy the following link and paste them to the browser address bar to activate your account: 
+        " + activate_link,
+      "<p>Welcome to Max's Online Bookstore, " + name + "</p>
+        <p>You have recently registered account " + account + "</p>
+        <p>Please click the following link to activate your account: </p>
+        <a href='" + activate_link + "'> Reset Password </a>"
+      )
+  end
+
+  def send_password_reset_email(name, account, email_address, reset_link)
+    send_email(email_address, 
+      "Max's Online Bookstore Password Reset",
+      "Welcome to Max's Online Bookstore, " + name + "
+        Please copy the following link and paste them to the browser address bar to reset your password: 
+        " + reset_link,
+      "<p>Welcome to Max's Online Bookstore, " + name + "</p>
+        <p>You have recently requested to reset your password of account " + account + "</p>
+        <p>Please click the following link to reset your password: </p>
+        <a href='" + reset_link + "'> Reset Password </a>"
+      )
+  end
+
+  def send_email(email_address, subj, text, html)
     gmail = Gmail.new("ngoc.nguyen@stanyangroup.com", "hongngoc92")
       gmail.deliver do
         to email_address 
-        subject "Max's Online Bookstore Account Activation"
+        subject subj
         text_part do
-          body "Welcome to Max's Online Bookstore, " + name + "
-          Please copy the following link and paste them to the browser address bar to activate your account: 
-          " + activate_link
+          body text
         end
         html_part do
-          body "<p>Welcome to Max's Online Bookstore, " + name + "</p>
-          <p>You have recently registered account " + account + "</p>
-          <p>Please click the following link to activate your account: </p>
-          <a href='" + activate_link + "'> Activate Account </a>"
+          body html
         end
       end
   end
