603eb1d85106aebd84460026a3c2b183cbee0e1a
maxdota <sieu_ngoc_2007@yahoo.com>
2013-10-09 09:54:28 +0700

    adding upgrade and downgrade test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,29 +4,30 @@ describe UsersController do
   
   before do
     User.delete_all
-    @user = FactoryGirl.create(:user)
+    @admin = FactoryGirl.create(:user)
+    @user = FactoryGirl.create(:user, :role => "normal")
   end
 
   context "Reset password" do
     it "resets password via token code" do      
-      get :activate, :token => @user.tokenized_code
-      @user = User.find(@user.id)
-      @user.tokenized_code.should == ""
-      post :password_reset, :user => {"username" => @user.username, "email_address" => @user.email_address}
-      @user = User.find(@user.id)      
-      @user.tokenized_code.should_not == ""
-      get :password_reset_result, :token => @user.tokenized_code
+      get :activate, :token => @admin.tokenized_code
+      @admin = User.find(@admin.id)
+      @admin.tokenized_code.should == ""
+      post :password_reset, :user => {"username" => @admin.username, "email_address" => @admin.email_address}
+      @admin = User.find(@admin.id)      
+      @admin.tokenized_code.should_not == ""
+      get :password_reset_result, :token => @admin.tokenized_code
       flash[:notice].should have_content("Correct token code.")
       post :password_reset_result, :user => {"password" => "new", "password_confirmation" => "old"}   
       flash[:notice].should have_content("Password confirmation does not match the password.")
-      @user = User.find(@user.id)      
-      @user.tokenized_code.should_not == ""
-      @user.authenticate("new").should == false
+      @admin = User.find(@admin.id)      
+      @admin.tokenized_code.should_not == ""
+      @admin.authenticate("new").should == false
       post :password_reset_result, :user => {"password" => "new", "password_confirmation" => "new"}   
       flash[:notice].should have_content("New password has been set.")      
-      @user = User.find(@user.id)      
-      @user.tokenized_code.should == ""      
-      @user.authenticate("new").should_not == false
+      @admin = User.find(@admin.id)      
+      @admin.tokenized_code.should == ""      
+      @admin.authenticate("new").should_not == false
     end
   end
   
@@ -37,16 +38,33 @@ describe UsersController do
     response.should redirect_to login_url
   end
   
+  context "Manage role" do
+    it "should raise error when downgrading the last admin" do
+      get :downgrade, :id => @admin.id
+      response.should redirect_to users_url
+      flash[:notice].should have_content("Can't delete/downgrade the last admin")
+    end
+    it "should upgrade normal user to admin" do
+      get :upgrade, :id => @user.id
+      @user = User.find(@user.id)
+      @user.role.should == "admin"
+    end
+    it "should downgrade admin to normal user" do
+      get :downgrade, :id => @admin.id
+      @admin = User.find(@admin.id)
+      @admin.role.should == "normal"
+    end
+  end
+  
   context "Delete" do
     it "should destroy user" do
-      @normal_user = FactoryGirl.create(:user, :role => "normal")
       count = User.count
-      delete :destroy, :id => @normal_user.id
+      delete :destroy, :id => @user.id
       User.count.should == count - 1
       response.should redirect_to users_url
     end
     it "should raise error when deleting the last admin" do
-      delete :destroy, :id => @user.id
+      delete :destroy, :id => @admin.id
       response.should redirect_to users_url
       flash[:notice].should have_content("Can't delete/downgrade the last admin")
     end
@@ -60,11 +78,11 @@ describe UsersController do
     end  
     
     it "activates user by token code" do
-      @user.activation.should == false
-      get :activate, :token => @user.tokenized_code
+      @admin.activation.should == false
+      get :activate, :token => @admin.tokenized_code
       flash[:notice].should have_content("Activation completed.")
-      @user = User.find(@user.id)
-      @user.activation.should == true
+      @admin = User.find(@admin.id)
+      @admin.activation.should == true
     end
   end
 end
